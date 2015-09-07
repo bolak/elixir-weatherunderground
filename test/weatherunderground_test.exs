f5c2c7be32d424f9b91602b8ce94f2b26d712b32
Sean Bolak <seanbolak@gmail.com>
2015-09-06 22:34:56 -0400

    updated unit tests

--- test/weatherunderground_test.exs
+++ test/weatherunderground_test.exs
@@ -8,13 +8,9 @@ defmodule WeatherUndergroundTest do
     assert 1 + 1 == 2
   end
 
-  test "city_conditions united states" do
+  test "city_conditions" do
     options = %{ client_key: "c53f1849879a17da", state: "FL", city: "Orlando" }
     assert {:ok, _} = WeatherUnderground.city_conditions(options)
   end
 
-  test "city_conditions international" do
-    options = %{ client_key: "c53f1849879a17da", city: "Oviedo" }
-    assert {:ok, _} = WeatherUnderground.city_conditions(options)
-  end
 end
